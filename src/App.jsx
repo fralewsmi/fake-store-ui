d64cf5481bbc3af4d9db80bb9a8844fc62598bb2
Fraser Lewis-Smith <lewissmith.fraser@gmail.com>
2023-04-25 14:56:26 +1200

    Add First Product Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
+import Product from "./components/product/Product";
+
 const App = () => (
-  <div className="bg-slate-200 h-screen">
+  <div className="bg-slate-200 h-screen flex flex-col">
     <header className="bg-slate-800">
-      <h1 className="text-white text-4xl text-center font-bold py-2">
+      <h1 className="text-white text-4xl text-center font-bold py-4">
         Fake Store
       </h1>
     </header>
-    <main className="container mx-auto bg-white h-full"></main>
+    <main className="container mx-auto bg-white grow overflow-y-scroll">
+      <h2 className="text-2xl text-center font-bold py-4">Products</h2>
+      <section className="flex flex-col items-center gap-4 py-8">
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+        <Product
+          title="test"
+          price="$100"
+          description="description"
+          image="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80"
+        />
+      </section>
+    </main>
   </div>
 );
 
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const Product = ({ title, price, description, image }) => {
+  return (
+    <div className="w-1/2 rounded shadow-lg hover:bg-sky-100">
+      <div className="flex">
+        <div className="shrink-0">
+          <img class="h-32 w-full object-cover" src={image} alt={title} />
+        </div>
+        <div className="p-4">
+          <h3 className="text-sky-600 font-bold">{title}</h3>
+          <h4 className="text-xl font-bold">{price}</h4>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Product.propTypes = {
+  title: PropTypes.string,
+  price: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+};
+
+export default Product;
