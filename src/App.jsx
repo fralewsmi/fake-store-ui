a052e47c7384b7f7ff7698318ddaae5747391b84
Fraser Lewis-Smith <lewissmith.fraser@gmail.com>
2023-04-25 13:26:18 +1200

    Add tailwindcss and scaffold out initial layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => (
+  <div className="bg-slate-200 h-screen">
+    <header className="bg-slate-800">
+      <h1 className="text-white text-4xl text-center font-bold py-2">
+        Fake Store
+      </h1>
+    </header>
+    <main className="container mx-auto bg-white h-full"></main>
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
